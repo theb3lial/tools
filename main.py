9f20e696f9c78dc4563db2e7086613640546ea66
b3lial <65880612+theb3lial@users.noreply.github.com>
2021-07-16 20:55:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,39 +11,39 @@ fCiano = '\033[1;36m'
 fReset = '\033[m'
 
 def tool():
-    print("""{}
+    print(fVermelho + f"""
        __________
-  {}<_______ /
-   {}<______/  /(_
-    {}<____<  ( __\\
-     {}<____\  \ \\
-     {}<_____\\\_) )  {}b3lial
-    {}/          /
-   {}/ /_|  )___/
-  {}/ |  / /
- {}/  |   \\\__
+  {fVerde}<_______ /
+   {fAzul}<______/  /(_
+    {fAmarelo}<____<  ( __\\
+     {fBranco}<____\  \ \\
+     {fVermelho}<_____\\\_) )  {fReset}b3lial
+    {fVerde}/          /
+   {fAzul}/ /_|  )___/
+  {fAmarelo}/ |  / /
+ {fReset}/  |   \\\__
 ~~~~    (~~~)
 
-01 {}> {}sqlmap          07 {}> {}Evil-Droid          99 {}> {}Exit
-02 {}> {}dotdotpwn       08 {}> {}AdvPhishing
-03 {}> {}BlackWidow      09 {}> {}slowloris
-04 {}> {}sherlock        10 {}> {}One-Lin3r
-05 {}> {}BlackHydra      11 {}> {}PwnXSS
-06 {}> {}track-ip        12 {}> {}PhoneInfoga\n""".format(fVermelho, fVerde, fAzul, fAmarelo, fBranco, fVermelho, fReset, fVerde, fAzul, fAmarelo, fReset, fCiano, fReset, fCiano, fReset, fCiano, fReset, fCiano, fReset, fCiano, fReset, fCiano, fReset, fCiano, fReset, fCiano, fReset, fCiano, fReset, fCiano, fReset, fCiano, fReset, fCiano, fReset, fCiano, fReset))
+01 {fCiano}> {fReset}sqlmap          07 {fCiano}> {fReset}Evil-Droid          99 {fCiano}> {fReset}Exit
+02 {fCiano}> {fReset}dotdotpwn       08 {fCiano}> {fReset}AdvPhishing
+03 {fCiano}> {fReset}BlackWidow      09 {fCiano}> {fReset}slowloris
+04 {fCiano}> {fReset}sherlock        10 {fCiano}> {fReset}One-Lin3r
+05 {fCiano}> {fReset}BlackHydra      11 {fCiano}> {fReset}PwnXSS
+06 {fCiano}> {fReset}track-ip        12 {fCiano}> {fReset}PhoneInfoga\n""")
 
 def download():
-    print("""{}
-       __________
-  {}<_______ /
-   {}<______/  /(_
-    {}<____<  ( __\\
-     {}<____\  \ \\
-     {}<_____\\\_) )  {}b3lial
-    {}/          /
-   {}/ /_|  )___/
-  {}/ |  / /
- {}/  |   \\\__
-~~~~    (~~~)\n""".format(fVermelho, fVerde, fAzul, fAmarelo, fBranco, fVermelho, fReset, fVerde, fAzul, fAmarelo, fReset))
+    print(fVermelho + f"""
+           __________
+      {fVerde}<_______ /
+       {fAzul}<______/  /(_
+        {fAmarelo}<____<  ( __\\
+         {fBranco}<____\  \ \\
+         {fVermelho}<_____\\\_) )  {fReset}b3lial
+        {fVerde}/          /
+       {fAzul}/ /_|  )___/
+      {fAmarelo}/ |  / /
+     {fReset}/  |   \\\__
+    ~~~~    (~~~)\n""")
 
 def clear():
     os.system(["clear", "cls"][os.name == "nt"])
@@ -53,85 +53,85 @@ try:
         clear()
         time.sleep(1)
         tool()
-        insert = str(input("{}λ {}".format(fCiano, fReset)))
+        insert = str(input(f"{fCiano}λ {fReset}"))
         if insert == "1" or insert == "01":
             clear()
             time.sleep(1)
             download()
-            os.system("cd $HOME&&git clone https://github.com/sqlmapproject/sqlmap")
-            input("\nPressione ENTER para voltar ")
+            os.system(f"cd $HOME&&git clone https://github.com/sqlmapproject/sqlmap")
+            input(f"\nPressione ENTER para voltar ")
         elif insert == "2" or insert == "02":
             clear()
             time.sleep(1)
             download()
-            os.system("cd $HOME&&git clone https://github.com/wireghoul/dotdotpwn")
-            input("\nPressione ENTER para voltar ")
+            os.system(f"cd $HOME&&git clone https://github.com/wireghoul/dotdotpwn")
+            input(f"\nPressione ENTER para voltar ")
         elif insert == "3" or insert == "03":
             clear()
             time.sleep(1)
             download()
-            os.system("cd $HOME&&git clone https://github.com/1N3/BlackWidow")
-            input("\nPressione ENTER para voltar ")
+            os.system(f"cd $HOME&&git clone https://github.com/1N3/BlackWidow")
+            input(f"\nPressione ENTER para voltar ")
         elif insert == "4" or insert == "04":
             clear()
             time.sleep(1)
             download()
-            os.system("cd $HOME&&git clone https://github.com/sherlock-project/sherlock")
-            input("\nPressione ENTER para voltar ")
+            os.system(f"cd $HOME&&git clone https://github.com/sherlock-project/sherlock")
+            input(f"\nPressione ENTER para voltar ")
         elif insert == "5" or insert == "05":
             clear()
             time.sleep(1)
             download()
-            os.system("cd $HOME&&git clone https://github.com/5l1v3r1/BlackHydra")
-            input("\nPressione ENTER para voltar ")
+            os.system(f"cd $HOME&&git clone https://github.com/5l1v3r1/BlackHydra")
+            input(f"\nPressione ENTER para voltar ")
         elif insert == "6" or insert == "06":
             clear()
             time.sleep(1)
             download()
-            os.system("cd $HOME&&git clone https://github.com/htr-tech/track-ip")
-            input("\nPressione ENTER para voltar ")
+            os.system(f"cd $HOME&&git clone https://github.com/htr-tech/track-ip")
+            input(f"\nPressione ENTER para voltar ")
         elif insert == "7" or insert == "07":
             clear()
             time.sleep(1)
             download()
-            os.system("cd $HOME&&git clone https://github.com/M4sc3r4n0/Evil-Droid")
-            input("\nPressione ENTER para voltar ")
+            os.system(f"cd $HOME&&git clone https://github.com/M4sc3r4n0/Evil-Droid")
+            input(f"\nPressione ENTER para voltar ")
         elif insert == "8" or insert == "08":
             clear()
             time.sleep(1)
             download()
-            os.system("cd $HOME&&git clone https://github.com/Ignitetch/AdvPhishing")
-            input("\nPressione ENTER para voltar ")
+            os.system(f"cd $HOME&&git clone https://github.com/Ignitetch/AdvPhishing")
+            input(f"\nPressione ENTER para voltar ")
         elif insert == "9" or insert == "09":
             clear()
             time.sleep(1)
             download()
-            os.system("cd $HOME&&git clone https://github.com/gkbrk/slowloris")
-            input("\nPressione ENTER para voltar ")
+            os.system(f"cd $HOME&&git clone https://github.com/gkbrk/slowloris")
+            input(f"\nPressione ENTER para voltar ")
         elif insert == "10":
             clear()
             time.sleep(1)
             download()
-            os.system("cd $HOME&&git clone https://github.com/D4Vinci/One-Lin3r")
-            input("Pressione ENTER para voltar:")
+            os.system(f"cd $HOME&&git clone https://github.com/D4Vinci/One-Lin3r")
+            input(f"Pressione ENTER para voltar:")
         elif insert == "11":
             clear()
             time.sleep(1)
             download()
-            os.system("cd $HOME&&git clone https://github.com/pwn0sec/PwnXSS")
-            input("\nPressione ENTER para voltar ")
+            os.system(f"cd $HOME&&git clone https://github.com/pwn0sec/PwnXSS")
+            input(f"\nPressione ENTER para voltar ")
         elif insert == "12":
             clear()
             time.sleep(1)
             download()
-            os.system("cd $HOME&&git clone https://github.com/ExpertAnonymous/PhoneInfoga")
-            input("\nPressione ENTER para voltar ")
+            os.system(f"cd $HOME&&git clone https://github.com/ExpertAnonymous/PhoneInfoga")
+            input(f"\nPressione ENTER para voltar ")
         elif insert == "99":
-            print("Volte sempre.")
+            print(f"Volte sempre.")
             break
         else:
-            print("Opção inválida, tente novamente em 5 segundos...")
+            print(f"Opção inválida, tente novamente em 5 segundos...")
             time.sleep(5)
 
 except KeyboardInterrupt:
-    print("\nVolte sempre.")
+    print(f"\nVolte sempre.")
